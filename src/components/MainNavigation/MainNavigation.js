fe089089f8f146747b83470a400a8a14f2a9ad95
FlorinC <costandacheflorinadrian@gmail.com>
2022-08-03 15:27:33 +0300

    added a button in the navbar that redirects the user to the login page

--- src/components/MainNavigation/MainNavigation.js
+++ src/components/MainNavigation/MainNavigation.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { AuthContext } from "../../context/auth-context";
 import Logo from "../logo/Logo";
+import { Link } from "react-router-dom";
 
 import {
   StyledNav,
@@ -8,6 +9,7 @@ import {
   StyledActionsArea,
 } from "./MainNavigation.styled";
 import LogoutButton from "../LogoutButton/LogoutButton";
+import Text from "../UIElements/Typography/Text";
 
 const MainNavigation = (props) => {
   const auth = useContext(AuthContext);
@@ -15,13 +17,19 @@ const MainNavigation = (props) => {
   return (
     <StyledNav>
       <StyledLogoArea>
-        
-
         <Logo />
       </StyledLogoArea>
 
       <StyledActionsArea>
         {auth.isLoggedIn && <LogoutButton />}
+        {!auth.isLoggedIn && (
+          <Link to="/autentificare">
+            {" "}
+            <Text type="text" size="medium" bold="true">
+              Autentificare
+            </Text>{" "}
+          </Link>
+        )}
       </StyledActionsArea>
     </StyledNav>
   );
